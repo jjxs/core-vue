70ff302fa6f2941a5175bbf4c126b85abfb83229
jjxs <957990586@gmail.com>
2022-06-02 17:07:59 +0800

    ref isRef unRef

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -46,7 +46,7 @@ function cleanupEffect(effect) {
   effect.deps.length = 0;
 }
 
-function isTracking(){
+export function isTracking(){
   return shouldTrack && activeEffect !== undefined;
 }
 
@@ -68,11 +68,16 @@ export function track(target, key) {
     depsMap.set(key, dep)
   }
   
+  trackEffects(dep);
+}
+
+export function trackEffects(dep){
   if (dep.has(activeEffect)) return;
 
   dep.add(activeEffect)
   activeEffect.deps.push(dep)
 }
+
 
 /**
  * 触发依赖
@@ -82,6 +87,10 @@ export function track(target, key) {
 export function trigger(target, key) {
   let depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
+  triggerEffects(dep)
+}
+
+export function triggerEffects(dep){
   for (const effect of dep) {
     if(effect.scheduler){
       effect.scheduler()
@@ -90,7 +99,6 @@ export function trigger(target, key) {
     }
   }
 }
-
 
 
 /**
---
+++ src/reactivity/ref.ts
@@ -0,0 +1,52 @@
+import { hasChanged, isObject } from "../shared";
+import { trackEffects, triggerEffects, isTracking } from "./effect";
+import { reactive } from "./reactive";
+
+class RefTmpl {
+    private _value :any;
+    private dep;
+    private _rawValue :any;
+    private __v_isRef = true;
+    constructor(value) {
+        this._rawValue = value;
+        this._value = convert(value)
+        this.dep = new Set()
+    }
+
+    get value(){
+        trackRefValue(this)
+        return this._value
+    }
+
+    set value(newValue){
+        if(hasChanged(this._rawValue, newValue)){
+            this._rawValue = newValue
+            this._value = convert(newValue)
+            triggerEffects(this.dep)
+        }
+    }
+}
+
+function convert(value){
+    return isObject(value) ? reactive(value) : value
+}
+
+function trackRefValue(ref){
+    if(isTracking()){
+        trackEffects(ref.dep)
+    }
+}
+
+export function ref(value) {
+    return new RefTmpl(value)
+};
+
+
+export function isRef(ref) {
+    return !!ref.__v_isRef
+};
+
+export function unRef(ref) {
+    return isRef(ref)? ref.value : ref
+};
+
