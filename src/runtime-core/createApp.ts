1ecb4ae17127752f2571e0b95ebb3e14a86a012e
jjxs <957990586@gmail.com>
2022-06-11 16:32:23 +0800

    实现初始化 component 主流程

---
+++ src/runtime-core/createApp.ts
@@ -0,0 +1,11 @@
+import { createVNode } from "./vnode";
+
+export function createApp(rootComponent) {
+    return {
+        mount(rootComponent) {
+            const vnode = createVNode(rootComponent);
+
+            render(vnode, rootComponent)
+        }
+    }
+};
