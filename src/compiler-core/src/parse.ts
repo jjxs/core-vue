f4f2d505b096e3316e8fb8d7558531f0991fe799
jjxs <957990586@gmail.com>
2022-10-20 15:10:03 +0800

    实现解析 element 功能

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+    Start,
+    End
+}
+
 export function baseParse(content: string) {
     const context = createParserContext(content)
     return createRoot(paserChildren(context))
@@ -9,8 +14,13 @@ function paserChildren(context) {
     const nodes: any = []
     let node;
 
-    if(context.source.startsWith("{{")){
+    const s = context.source;
+    if(s.startsWith("{{")){
         node = paserInterpolation(context)
+    } else if (s[0] === "<") {
+        if(/[a-z]/i.test(s[1])){
+            node = parseElement(context)
+        }
     }
     nodes.push(node);
     return nodes
@@ -33,9 +43,6 @@ function paserInterpolation(context) {
 
     advanceBy(context, rawContentLength + closeDelimiter.length)
 
-    console.log(rawContent);
-    console.log("context.source", context.source);
-    console.log(closeIndex);
     return {
             type: NodeTypes.INTERPOLATION,
             content: {
@@ -60,3 +67,30 @@ function createParserContext(content: string):any {
 function advanceBy(context: any, length: number) {
     context.source = context.source.slice(length)
 }
+function parseElement(context: any) {
+    // Implement
+    // 1. 解析 tag
+    
+    const element = parseTag(context, TagType.Start)
+    parseTag(context, TagType.End)
+    console.log("_______________________", context.source);
+
+    return element;
+}
+
+function parseTag(context: any, type: TagType) {
+    const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+    console.log(match);
+    const tag = match[1];
+    // 2. 删除处理完成的代码
+    advanceBy(context, match[0].length)
+    advanceBy(context, 1)
+
+    if (type === TagType.End) return;
+
+    return {
+        type: NodeTypes.ELEMENT,
+        tag: tag
+    }
+}
+
