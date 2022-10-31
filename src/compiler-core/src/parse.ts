09830c39bd5a92f388436ae1049f71f4a1929b55
jjxs <957990586@gmail.com>
2022-10-31 17:40:02 +0800

    实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,117 +1,153 @@
 import { NodeTypes } from "./ast";
 
 const enum TagType {
-    Start,
-    End
+  Start,
+  End,
 }
 
 export function baseParse(content: string) {
-    const context = createParserContext(content)
-    return createRoot(paserChildren(context))
-};
+  const context = createParserContext(content);
+  return createRoot(parseChildren(context, []));
+}
 
-function paserChildren(context) {
-    const nodes: any = []
+function parseChildren(context, ancestors) {
+  const nodes: any = [];
+
+  while (!isEnd(context, ancestors)) {
     let node;
-
     const s = context.source;
-    if(s.startsWith("{{")){
-        node = paserInterpolation(context)
+    if (s.startsWith("{{")) {
+      node = parseInterpolation(context);
     } else if (s[0] === "<") {
-        if(/[a-z]/i.test(s[1])){
-            node = parseElement(context)
-        }
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors);
+      }
     }
 
-    if(!node){
-        node = parseText(context)
+    if (!node) {
+      node = parseText(context);
     }
+
     nodes.push(node);
-    return nodes
+  }
+  return nodes;
 }
 
-function parseText(context: any): any {
-    // 1. 获取content
-    const content = parseTextData(context, context.source.length);
-    return {
-        type: NodeTypes.TEXT,
-        content: content
+function isEnd(context, ancestors) {
+  const s = context.source;
+  if (s.startsWith("</")) {
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      const tag = ancestors[i].tag;
+      if (startsWithEndTagOpen(s, tag)) {
+        return true;
+      }
     }
+  }
+  return !s;
 }
 
+function parseText(context: any) {
+  let endIndex = context.source.length;
+  let endTokens = ["<", "{{"];
+
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i]);
+    if (index !== -1 && endIndex > index) {
+      endIndex = index;
+    }
+  }
+
+  const content = parseTextData(context, endIndex);
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  };
+}
 
 function parseTextData(context: any, length) {
-    const content = context.source.slice(0, length);
+  const content = context.source.slice(0, length);
 
-    // 2. 推进
-    advanceBy(context, length);
-    return content;
+  advanceBy(context, length);
+  return content;
 }
 
-function paserInterpolation(context) {
+function parseElement(context: any, ancestors) {
+  const element: any = parseTag(context, TagType.Start);
+  ancestors.push(element);
+  element.children = parseChildren(context, ancestors);
+  ancestors.pop();
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End);
+  } else {
+    throw new Error(`缺少结束标签:${element.tag}`);
+  }
 
-    const openDelimiter = "{{"
-    const closeDelimiter = "}}"
-
-    const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length)
-    
-    advanceBy(context, openDelimiter.length)
-
-    const rawContentLength = closeIndex - openDelimiter.length;
-
-    const rawContent = parseTextData(context, rawContentLength)
-
-    const content = rawContent.trim()
-
-    advanceBy(context, closeDelimiter.length)
-
-    return {
-            type: NodeTypes.INTERPOLATION,
-            content: {
-                type: NodeTypes.SIMPLE_EXPRESSION,
-                content: content ,
-            },    
-        }    
-}    
-
-function createRoot(children) {
-    return {
-        children
-    }    
-}
-    
-function createParserContext(content: string):any {
-    return {
-        source: content
-    }    
+  return element;
 }
 
-function advanceBy(context: any, length: number) {
-    context.source = context.source.slice(length)
-}
-function parseElement(context: any) {
-    // Implement
-    // 1. 解析 tag
-    
-    const element = parseTag(context, TagType.Start)
-    parseTag(context, TagType.End)
-    console.log("_______________________", context.source);
-
-    return element;
+function startsWithEndTagOpen(source, tag) {
+  return (
+    source.startsWith("</") &&
+    source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+  );
 }
 
 function parseTag(context: any, type: TagType) {
-    const match: any = /^<\/?([a-z]*)/i.exec(context.source)
-    console.log(match);
-    const tag = match[1];
-    // 2. 删除处理完成的代码
-    advanceBy(context, match[0].length)
-    advanceBy(context, 1)
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
 
-    if (type === TagType.End) return;
+  if (type === TagType.End) return;
 
-    return {
-        type: NodeTypes.ELEMENT,
-        tag: tag
-    }
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
+}
+
+function parseInterpolation(context) {
+  // {{message}}
+  const openDelimiter = "{{";
+  const closeDelimiter = "}}";
+
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+
+  advanceBy(context, openDelimiter.length);
+
+  const rawContentLength = closeIndex - openDelimiter.length;
+
+  const rawContent = parseTextData(context, rawContentLength);
+
+  const content = rawContent.trim();
+
+  advanceBy(context, closeDelimiter.length);
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content,
+    },
+  };
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context.source.slice(length);
+}
+
+function createRoot(children) {
+  return {
+    children,
+  };
+}
+
+function createParserContext(content: string): any {
+  return {
+    source: content,
+  };
 }
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -23,14 +23,15 @@ describe("parse", () => {
                 expect(ast.children[0]).toStrictEqual(
                     {
                         type: NodeTypes.ELEMENT,
-                        tag: "div"
+                        tag: "div",
+                        children: []
                     }
                 )
         })
     })
 
     describe("text", () => {
-        test("simple text", () => {
+        it("simple text", () => {
             const ast = baseParse("some text");
                 expect(ast.children[0]).toStrictEqual(
                     {
@@ -40,5 +41,61 @@ describe("parse", () => {
                 )
         })
     })
-})
 
+    test("hello world", () => {
+        const ast = baseParse("<div>hi,{{message}}</div>");
+    
+        expect(ast.children[0]).toStrictEqual({
+          type: NodeTypes.ELEMENT,
+          tag: "div",
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: "hi,",
+            },
+            {
+              type: NodeTypes.INTERPOLATION,
+              content: {
+                type: NodeTypes.SIMPLE_EXPRESSION,
+                content: "message",
+              },
+            },
+          ],
+        });
+      });
+    
+    test ("Nested Element", () => {
+        const ast = baseParse("<div><p>hi</p>{{message}}</div>");
+    
+        expect(ast.children[0]).toStrictEqual({
+          type: NodeTypes.ELEMENT,
+          tag: "div",
+          children: [
+
+            {
+              type: NodeTypes.ELEMENT,
+              tag: "p",
+              children: [{
+                type: NodeTypes.TEXT,
+                content: "hi"
+                }],
+            },
+            {
+              type: NodeTypes.INTERPOLATION,
+              content: {
+                type: NodeTypes.SIMPLE_EXPRESSION,
+                content: "message",
+              },
+            },
+
+          ],
+        });
+    });
+
+    test("should throw error when lack and tag", () => {
+      expect(() => {
+        baseParse("<div><span></div>")
+
+      }).toThrow("缺少结束标签:span")
+    })
+})
