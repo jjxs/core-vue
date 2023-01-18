9eda756a6ea16189600e5d37c483d169474e27a8
fz <957990586@qq.com>
2023-01-18 11:18:32 +0800

    实现代码生成插值类型

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,10 +1,12 @@
 import { NodeTypes } from "./ast";
+import { TO_DISPLAY_STRING } from "./runtimeHelpers";
 
 export function transform(root, options = {}) {
     const context = createTransformContext(root, options)
     traverseNode(root, context);
-
     createRootCodegen(root)
+
+    root.helpers = [...context.helpers.keys()]
 }
 
 function traverseNode(node: any, context) {
@@ -16,25 +18,36 @@ function traverseNode(node: any, context) {
         
     }
 
-    const children = node.children
-
-    traverseChildren(children, context);
-    
+    switch (node.type) {
+        case NodeTypes.INTERPOLATION:
+            context.helper(TO_DISPLAY_STRING)
+            break;
+        case NodeTypes.ROOT:
+        case NodeTypes.ELEMENT:
+            traverseChildren(node, context);
+            break;
+        default:
+            break;
+    }
 }
 
-function traverseChildren(children: any, context: any) {
-    if (children) {
-        for (let i = 0; i < children.length; i++) {
-            const node = children[i];
-            traverseNode(node, context);
-        }
+function traverseChildren(node: any, context: any) {
+    const children = node.children;
+    
+    for (let i = 0; i < children.length; i++) {
+        const node = children[i];
+        traverseNode(node, context);
     }
 }
 
 function createTransformContext(root: any, options: any) {
     const context = {
         root,
-        nodeTransforms: options.nodeTransforms || []
+        nodeTransforms: options.nodeTransforms || [],
+        helpers: new Map(),
+        helper(key){
+            context.helpers.set(key, 1)
+        }
     }
 
     return context;
