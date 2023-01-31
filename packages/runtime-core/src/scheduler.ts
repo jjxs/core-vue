63fdacff41af09b4f156198d617d63921a9443c5
fz <957990586@qq.com>
2023-01-31 15:14:58 +0800

    实现 watchEffect

--- packages/runtime-core/src/scheduler.ts
+++ packages/runtime-core/src/scheduler.ts
@@ -1,9 +1,9 @@
 const queue: any[] = [];
-
+const activePreFlushCbs: any[] = [];
 const p = Promise.resolve();
 let isFlushPending = false;
 
-export function nextTick(fn) {
+export function nextTick(fn?) {
   return fn ? p.then(fn) : p;
 }
 
@@ -15,6 +15,12 @@ export function queueJobs(job) {
 
   queueFlush();
 }
+
+export function queuePreFlushCb(job) {
+  activePreFlushCbs.push(job);
+  queueFlush();
+}
+
 function queueFlush() {
   if (isFlushPending) return;
   isFlushPending = true;
@@ -24,8 +30,15 @@ function queueFlush() {
 
 function flushJobs() {
   isFlushPending = false;
+
+  flushPreFlushCbs();
   let job;
   while ((job = queue.shift())) {
     job && job();
+  }
+}
+function flushPreFlushCbs() {
+  for (let i = 0; i < activePreFlushCbs.length; i++) {
+    activePreFlushCbs[i]();
   }
 }
